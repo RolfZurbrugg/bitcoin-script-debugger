66e84205ead59493b2c5b39f65ad23890fcca0ba
RolfZurbrugg <rolf.zurbrugg@gmail.com>
2017-10-30 18:38:48 +0100

    added 2 custom script and evalueted them one to true and one to false

--- webpage/app.js
+++ webpage/app.js
@@ -1,7 +1,6 @@
 var bitcore = require('bitcore-lib');
 var Mnemonic = require('bitcore-mnemonic');
 console.log('app.js active');
-//var buf =
 
 
 
@@ -9,9 +8,55 @@ var script = bitcore.Script()
     .add('OP_IF')                       // add an opcode by name
     .prepend(114)                       // add OP_2SWAP by code
     .add(bitcore.Opcode.OP_NOT)                 // add an opcode object
-    .add(new bitcore.deps.Buffer('bacacafe', 'hex')) // add a data buffer (will append the size of the push operation first)
+    .add(new bitcore.deps.Buffer('bacacafe', 'hex')); // add a data buffer (will append the size of the push operation first)
 
-var test = bitcore.utils.assert(script.toString() === 'OP_2SWAP OP_IF OP_NOT 4 0xbacacafe');
+try{
+    assert(script.toString() === 'OP_2SWAP OP_IF OP_NOT 4 0xbacacafe');
+}
+catch (err){
+    console.log(err);
+}
 
-console.log(test);
+/* This doesnt work but why?
+try{
+    bitcore.utils.assert(script.toString() === 'OP_2SWAP OP_IF OP_NOT 4 0xbacacafe');
+}
+catch (err){
+    console.log(err);
+}
+*/
 
+console.log('I am stupid');
+
+console.log(script);
+
+//this function stems from bitcore.utils but for some reason I cant access it here :/
+function assert(cond, msg) {
+    if (!cond)
+        throw new Error(msg || 'Assertion failed');
+}
+
+
+// Now lets see if we can get a script to evaluate 1 1 + -> 2
+// Evaluates to true because 2 >= 1 ==> TRUE
+
+var script_simple_add = bitcore.Script()
+                            .add('OP_1')
+                            .add('OP_1')
+                            .add('OP_ADD');
+
+var test_add = bitcore.Script.Interpreter().verify(script_simple_add);
+
+console.log(test_add);
+
+// Now lets see if we can get a script to evaluate 1 1 - -> 2
+// Evaluates to true because 0 <= 1 ==> FALSE
+
+var script_simple_add = bitcore.Script()
+    .add('OP_1')
+    .add('OP_1')
+    .add('OP_SUB');
+
+var test_sub = bitcore.Script.Interpreter().verify(script_simple_add);
+
+console.log(test_sub);
