0565e9838b89d616f5bcd185a8a14d246701f7b9
RolfZurbrugg <rolf.zurbrugg@gmail.com>
2017-11-14 18:57:59 +0100

    Added functionalety that allows the translation of op code numbers to their original text representation

--- webpage/accesStack.js
+++ webpage/accesStack.js
@@ -14,6 +14,21 @@ var bitcore = require('bitcore-lib');
     bitcore.Script.Interpreter.prototype.printStack = function (_this) {
         here = Object.assign(this, _this);
         var fRequireMinimal = (here.flags & bitcore.Script.Interpreter.SCRIPT_VERIFY_MINIMALDATA) !== 0;
+
+        // Read instruction
+        var pc = here.pc -1;
+        console.log(pc);
+        var chunk = here.script.chunks[pc];
+        //here.pc++;
+        //console.log(chunk)
+        var opcodenum = chunk.opcodenum;
+
+        var op_code = getKeyByValue(bitcore.Opcode.map, opcodenum);
+
+        window.stack_trace += 'Opcodenum: ' + op_code + '\n';
+
+        console.log(here);
+
         //added by rolf
         window.stack_trace += '\n' + '--------- start of stack --------' + '\n';
         console.log('--------- start of stack --------');
@@ -30,4 +45,14 @@ var bitcore = require('bitcore-lib');
         console.log('--------- end of stack --------');
         // console.log(this);
     }
+
+    var getKeyByValue = function( map, value ) {
+        for( var prop in map ) {
+            if( map.hasOwnProperty( prop ) ) {
+                if( map[ prop ] === value )
+                    return prop;
+            }
+        }
+    }
+
 }(bitcore.Script.Interpreter.prototype.step));
