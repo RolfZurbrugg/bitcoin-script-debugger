c02992affd2308e4a4ae102192b56358eb47e40c
Samuel Egger <samuel.egger7@gmail.com>
2017-12-20 18:41:08 +0100

    A lot

---
+++ old/webpage/parser.js
@@ -0,0 +1,386 @@
+var bitcore = require('bitcore-lib');
+
+/**
+ * Enabeling this override allows signatures to be generated, but an error is caused when using these signatures.
+ * The reason as to why this happens is unknown at this point.
+ *
+ * here we override the bitcore Input.PublicKey.prototype.getSignature function.
+ * This function can be found int the bitcore-lib under /lib/transaction/input/publickey.js
+ * We need to override this function because we are using a dummy transaction to create all our signatures.
+ * The original function would verify if the public key would mach the one in the transaction.
+ * Of course our public key derived from the privet key will not match the pub key in the dummy transaction.
+ */
+// (function () {
+//     //define override method.
+//     bitcore.Transaction.Input.PublicKey.prototype.getSignatures = function(transaction, privateKey, index, sigtype) {
+//         //$.checkState(this.output instanceof bitcore.Transaction.Output);
+//         sigtype = sigtype || Signature.SIGHASH_ALL;
+//         var publicKey = privateKey.toPublicKey();
+//         if (true) { //changeing condition from (publicKey.toString() === this.output.script.getPublicKey().toString('hex'))
+//             return [new bitcore.Transaction.Signature.prototype.constructor({
+//                 publicKey: publicKey,
+//                 prevTxId: this.prevTxId,
+//                 outputIndex: this.outputIndex,
+//                 inputIndex: index,
+//                 signature: bitcore.Transaction.Sighash.sign(transaction, privateKey, sigtype, index, this.output.script),
+//                 sigtype: sigtype
+//             })];
+//         }
+//         return [];
+//     };
+//     console.log('override: bitcore.Transaction.Input.PublicKey.prototype.getSignatures successful');
+// })();
+
+
+
+
+/**
+ * The parser takes a string containing opcodes which are either separated by white spaces or
+ * carriage return. It will return a bitcore script object containing the script.
+ *
+ * ToDo extend this library to be able to do syntax checks
+ */
+
+
+
+var _numOfSigs = 1; //this variable is used to keep track of how many signatures are created and increment their variable name correctly
+
+(function (global) {
+    
+    var Parser = function (script_text) {
+        return new Parser.init(script_text); //ToDo why new parser
+    };
+
+
+    Parser.init = function (script_text) {
+
+        var opcode_arr = script_text.split(/\s+|\r+/); //spliting text on whitespace or new line. Trailing white spaces or new lines will cause an error
+
+        var script = new bitcore.Script();
+
+        if(script_text === ''){ //if a script is empty return a empty script
+            return script;
+        }
+
+        for(var i=0; i<opcode_arr.length; i++){
+            // if a value in the opcode_arr corresponds to a variable -> replace variable
+            if (/(privK_[0-9])/.test(opcode_arr[i])   || //test for private key variable
+                /(pubK_[0-9])/.test(opcode_arr[i])    || //test for public key variable
+                /(addr_[0-9])/.test(opcode_arr[i])){     //test for address
+
+
+                var variable = P$.getValueByKey(opcode_arr[i]); //ToDo find a better name instead of variable
+                script.add(variable.toBuffer());
+
+            }
+            else if (/(hash_[0-9])/.test(opcode_arr[i])){ //test for pubkik key hash. pubkey hash is already a buffer. testing for the key word hash_<number>
+                var variable = P$.getValueByKey(opcode_arr[i]);
+                script.add(variable);
+            }
+            else if (/(sig_[0-9])/.test(opcode_arr[i])){ //test for key word sig_<number>
+               var sig = P$.getValueByKey(opcode_arr[i]);
+
+               var scriptContainingSig = bitcore.Script.buildPublicKeyIn(sig.signature.toDER());
+                console.log('sigscript');
+                console.log(scriptContainingSig);
+                console.log(scriptContainingSig.toString());
+                script.add(scriptContainingSig);
+            }
+            else if (/(str_[0-9])/.test(opcode_arr[i])){ // test for a string variable
+                var str = P$.getValueByKey(opcode_arr[i]);
+                var strBuf = P$.convertStringToBuffer(str);
+                script.add(strBuf);
+            }
+            else if ((/(^[0-9])/).test(opcode_arr[i])){ //test for a number. ^ denotes that the string must start with a number. [0-9]* will then match any following numbers.
+                var num = Number(opcode_arr[i]); //convert the string to a number
+                script.add(bitcore.Opcode.smallInt(num));
+            }
+            else {
+                script.add(opcode_arr[i]);
+            }
+        }
+
+        console.log(script);
+        console.log(script.toString());
+        return script;
+
+
+    }
+
+    /**
+     * Methods for interaction with the stackArray
+     */
+
+    Parser.getStackArray = function (){
+        return Parser.prototype.stackArray;
+    }
+
+
+    /**
+     * methods for interaction with the variableMap
+      */
+
+    /**
+     * given a key returns the corresponding value
+     * @param key {string}
+     * @returns {*}
+     */
+    Parser.getValueByKey = function(key){
+        return Parser.prototype.variableMap[key];
+    };
+
+
+
+    /**
+     * returns the variableMap
+     * @returns {{}}
+     */
+    Parser.getVariableMap = function () {
+      return Parser.prototype.variableMap;
+    };
+
+    /**
+     * This function returns the variable map as an array.
+     * @returns {Array}
+     */
+    Parser.getVariableMapAsArray = function(){
+        return  convertMapToArray(P$.getVariableMap());
+    };
+
+    /**
+     * helper function, it converts a given map to an array.
+     * @param map
+     * @returns {Array}
+     */
+    function convertMapToArray(map){
+        var keyVal;
+        var value;
+        var count = 0;
+        var array = new Array();
+
+        Object.keys(map).forEach(function(key){ //iterate over all key value pairs in the map
+            array[count] = new Array();
+            keyVal = key; // extract the value of the key
+            value = map[key]; // extract the value corresponding to the given key.
+
+            array[count][0] = keyVal;
+            array[count][1] = value;
+
+            count++;
+        });
+
+        return array;
+    }
+
+    /**
+     * allow the dynamic addition of key value pairs
+     * @param key {string}
+     * @param value {object}
+     */
+    Parser.addKeyValuePair = function(key, value){
+        Parser.prototype.variableMap[key] = value;
+    };
+
+    /**
+     * deletes the key value pair for a given key
+     * @param key {string}
+     */
+    Parser.deleteKeyValuePair = function (key) {
+        delete Parser.prototype.variableMap[key];
+    };
+
+    /**
+     * This is a helper function, that creates a utox (unspent transaction output)
+     * @param outputScript
+     * @param toAddress
+     * @returns {*}
+     */
+    Parser.createUtox = function (outputScript, pubK){
+
+        //var privk = new bitcore.PrivateKey();
+        //var pubk = new bitcore.PublicKey.fromPrivateKey(privk);
+
+        if(!outputScript.isStandard()){ //the problem is if a script is not standard the bitcore client refuses to sign a transaction containing a non standard script.
+            outputScript = new bitcore.Script().add(pubK.toBuffer()).add('OP_CHECKSIG');
+            var debug =outputScript.isStandard();
+            if(!outputScript.isStandard()){
+                this.errstr = 'SCRITP IS NON STANDARD';
+            }
+        }
+
+        //creating the data object in order to be able to create a utox
+        var data = new Object(); // creating the data opbject to create an unspent tx
+        data.txid ='00baf6626abc2df808da36a518c69f09b0d2ed0a79421ccfde4f559d2e42128b'; // {String} the previous transaction id
+        //data.txId = '00baf6626abc2df808da36a518c69f09b0d2ed0a79421ccfde4f559d2e42128b'; // {String=} alias for 'txid'
+        //data.vout = 0; // {number} the index in the transaction
+        data.outputIndex = 0; // {number=} alias for 'vout'
+        //data.scriptPubKey = outputScript; // {string|Script} the script that must be resolved to release the funds
+        data.script = outputScript; // {string|Script=} alias for 'scriptPubKey' (Output Script)
+        //data.amount = 1; // {number} amount of bitcoins associated
+        data.satoshis =100000000; // {number=} alias for 'amount', but expressed in satoshis (1 BTC = 1e8 satoshis)
+        //data.address = toAddress; // {sting | Address=} the associated address to the script, if provided.
+
+        var utox = bitcore.Transaction.UnspentOutput(data);
+        return utox;
+    };
+
+    /**
+     *
+     * @param utox
+     * @param inputScript
+     * @param publicKey
+     * @returns {Transaction|Number}
+     */
+    Parser.createTransactionFromUtox = function (utox, publicKey){
+
+        //create a address from the public key
+        var address = publicKey.toAddress();
+
+        var transaction = bitcore.Transaction()
+            .from(utox,publicKey)
+            .to(address, 100000000);
+        /* further options list of options that can be specified in order to create transactions
+         * .change(changeAddress)
+         */
+
+        return transaction;
+    };
+
+    Parser.createTransaction = function(outputScript, privateKey){
+        var pubKey;
+        if(privateKey === Array){
+            pubKey = new Array();
+            for(var i=0; i<pubKeyArr.length; i++){
+                pubKey.add(privateKey[i].toPublicKey());
+            }
+        }else{
+            pubKey = privateKey.toPublicKey();
+        }
+
+        var utox = P$.createUtox(outputScript, pubKey); //the adress is not important for our purposes.
+        var tx = P$.createTransactionFromUtox(utox, pubKey);
+        P$.addKeyValuePair('tx', tx);
+    };
+
+    /**
+     * todo remove this, this is not usefull as each signature references the pubkey used to create the tx.
+     * @param tx
+     * @param privateKey
+     * @param option
+     */
+    // Parser.createSig = function (privateKey, option){
+    //     var tx = P$.getValueByKey('tx'); // get the dummy transaction to sign
+    //     var sigArray = tx.getSignatures(privateKey, option);
+    //     var sig = sigArray[0]; //at the moment only one signature is supported
+    //     P$.addKeyValuePair('sig_' + _numOfSigs, sig);
+    //     console.log('sig_' + _numOfSigs);
+    //     _numOfSigs ++;
+    // };
+
+
+    /**
+     * This function converts any string to a Uint8Array.
+     * @param string
+     * @returns {Uint8Array}
+     */
+    Parser.convertStringToBuffer = function (string){
+        var bytes = new bitcore.deps.Buffer(string);
+        return bytes;
+    };
+
+
+    Parser.test = function(){
+        console.log('test test');
+    };
+
+    Parser.init.prototype = Parser.prototype;
+
+    global.Parser = global.P$ = Parser;
+
+
+}(window));
+
+//todo not sure if this belongs inside the parser function.
+
+/**
+ * The map that is created here will be added to the Parses.prototype object.
+ * The parser proveides folowing methods for interaction with the map:
+ *      getValueByKey
+ *      getVariableMap
+ *      clearMap
+ *      addKeyValuePair
+ *      deleteKeyValuePair
+ *
+ * public keys will be denoted as <pubKey_n> where n is a the corresponding number.
+ * private keys will be denoted as <privKey_n>
+ * adresses will be denoted as <addr_n>
+ * signatures will be denoted as <sig> . no number is needed here because currently only one signature is suported. This might need to be expanded.
+ * @type {{}}
+ */
+
+(function () {
+    /**
+     * Initialize variable map with some default values
+     *
+     */
+    Parser.prototype.variableMap = {
+        SIGHASH_ALL:    bitcore.crypto.Signature.SIGHASH_ALL,
+        SIGHASH_NONE:   bitcore.crypto.Signature.SIGHASH_NONE,
+        SIGHASH_SINGLE: bitcore.crypto.Signature.SIGHASH_SINGLE,
+        SIGHASH_ANYONECANPAY: bitcore.crypto.Signature.SIGHASH_ANYONECANPAY
+    };
+
+    // creating a set of keys and addresses for use in default scripts
+    var privk0 = new bitcore.PrivateKey();
+    var pubk0 = new bitcore.PublicKey.fromPrivateKey(privk0);
+    var privk00 = new bitcore.PrivateKey();
+    var pubk00 = new bitcore.PublicKey.fromPrivateKey(privk00);
+    var privk000 = new bitcore.PrivateKey();
+    var pubk000 = new bitcore.PublicKey.fromPrivateKey(privk000);
+    var address = pubk0.toAddress();
+    var pubkHASH160 = bitcore.crypto.Hash.sha256ripemd160(pubk0.toBuffer());
+
+    P$.addKeyValuePair('privK_0',privk0);
+    P$.addKeyValuePair('pubK_0',pubk0);
+    P$.addKeyValuePair('privK_00',privk00);
+    P$.addKeyValuePair('pubK_00',pubk00);
+    P$.addKeyValuePair('privK_000',privk000);
+    P$.addKeyValuePair('pubK_000',pubk000);
+    P$.addKeyValuePair('addr_0', address);
+    P$.addKeyValuePair('hash_0',pubkHASH160);
+
+    var str = 'bitcoin rocks';
+    var strBuf = P$.convertStringToBuffer(str);
+    var hashStr = bitcore.crypto.Hash.sha256ripemd160(strBuf);
+
+    P$.addKeyValuePair('hash_00', hashStr);
+    P$.addKeyValuePair('str_0',str);
+
+    // this is usles because tx needs to be created with the pubkey belonging to the private key that signs it.
+    // /**
+    //  * creation of a dummy transaction which is used to create and verify signatures.
+    //  */
+    // var _pk = new bitcore.PrivateKey(); // values needed for creation of
+    // var _pubk = new bitcore.PublicKey.fromPrivateKey(_pk);
+    // var _addr = _pubk.toAddress();
+    // var _utox = P$.createUtox(bitcore.Script(),_addr); // create a dummy utox
+    // var _tx = P$.createTransactionFromUtox(_utox,_pubk);
+    // P$.addKeyValuePair('tx', _tx);
+    //
+    //
+    // /**
+    //  * create signatures for demo scripts
+    //  */
+    // var sigArray_0 = _tx.getSignatures(privk0,bitcore.crypto.Signature.SIGHASH_ALL);
+    // var sig_0 = sigArray_0[0];
+    // P$.addKeyValuePair('sig_0',sig_0);
+    //
+    // var sigArray_00 = _tx.getSignatures(privk00,bitcore.crypto.Signature.SIGHASH_ALL);
+    // var sig_00 = sigArray_00[0];
+    // P$.addKeyValuePair('sig_00',sig_00);
+
+    console.log('variable map initialized');
+
+})();
+
+
---
+++ old/webpage/prototypingWithTXSigneaturs.js
@@ -0,0 +1,380 @@
+var bitcore = require('bitcore-lib');
+var Mnemonic = require('bitcore-mnemonic');
+
+
+
+var privateKey = new bitcore.PrivateKey();
+var publicKey = new bitcore.PublicKey.fromPrivateKey(privateKey);
+var address = publicKey.toAddress();
+
+var privateKey2 = new bitcore.PrivateKey();
+var publicKey2 = new bitcore.PublicKey.fromPrivateKey(privateKey2);
+var changeAddress = publicKey2.toAddress();
+
+console.log('Private Key: '+privateKey.toString());
+console.log('Public Key: '+publicKey);
+console.log('Adress : '+address);
+
+
+
+//  building transaction chain on blocks, hope this works
+//
+// //creating a javascript block
+//
+// //genesis block hex string
+//
+// var genesisBlockHexString =
+//     "01000000" + //version
+//     "0000000000000000000000000000000000000000000000000000000000000000" + // prev block
+//     "3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a" + // merkle root
+//     "29ab5f49" + // timestamp
+//     "ffff001d" + //bits
+//     "1dac2b7c" + //nonce
+//     "01000000" + // version
+//     "01" + // number of transactions
+//     "01" + // inputs
+//     "0000000000000000000000000000000000000000000000000000000000000000" + // prev output
+//     "ffffffff" + // sequence
+//     "4d" + //script length
+//     "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73" + //scriptsig
+//     "ffffffff" + //sequence
+//     "01" + //outputs
+//     "00f2052a01000000" + //50 BTC
+//     "43" + // pk_script length
+//     "4104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac" + //pk script
+//     "00000000"; //lock time
+//
+//
+// var genesisBlockJSON = {
+//     "hash":"000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
+//     "ver":1,
+//     "prev_block":"0000000000000000000000000000000000000000000000000000000000000000",
+//     "mrkl_root":"4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+//     "time":1231006505,
+//     "bits":486604799,
+//     "nonce":2083236893,
+//     "n_tx":1,
+//     "size":285,
+//     "tx":[
+//         {
+//             "hash":"4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+//             "ver":1,
+//             "vin_sz":1,
+//             "vout_sz":1,
+//             "lock_time":0,
+//             "size":204,
+//             "in":[
+//                 {
+//                     "prev_out":{
+//                         "hash":"0000000000000000000000000000000000000000000000000000000000000000",
+//                         "n":4294967295
+//                     },
+//                     "coinbase":"04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73"
+//                 }
+//             ],
+//             "out":[
+//                 {
+//                     "value":"50.00000000",
+//                     "scriptPubKey":"04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f OP_CHECKSIG"
+//                 }
+//             ]
+//         }
+//     ],
+//     "mrkl_tree":[
+//         "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
+//     ]
+// };
+//
+// //var genesisBlockJSObj = JSON.parse(genesisBlockJSON);
+// var genesisBlockBuffer = bitcore.util.buffer.hexToBuffer(genesisBlockHexString);
+// var genesisBlock = bitcore.Block(genesisBlockBuffer);
+//
+// //var genesisBlock = bitcore.Block.fromString(genesisBlockHexString);
+//
+// console.log(genesisBlock);
+
+
+
+
+// // create an unspent transaction (utox)
+//
+// var data = new Object(); // creating the data opbject to create an unspent tx
+// data.txid ='00baf6626abc2df808da36a518c69f09b0d2ed0a79421ccfde4f559d2e42128b'; // {String} the previous transaction id
+// data.txId = ''; // {String=} alias for 'txid'
+// data.vout = 0; // {number} the index in the transaction
+// data.outputIndex = 0; // {number=} alias for 'vout'
+// data.scriptPubKey = ''; // {string|Script} the script that must be resolved to release the funds
+// data.script = ''; // {string|Script=} alias for 'scriptPubKey' (Output Script)
+// data.amount = 1; // {number} amount of bitcoins associated
+// data.satoshis =100000000; // {number=} alias for 'amount', but expressed in satoshis (1 BTC = 1e8 satoshis
+// data.address = ''; // {sting | Address=} the associated address to the script, if provided.
+//
+//
+// var unspentOutput = bitcore.Transaction.UnspentOutput(data);
+//
+// console.log(unspentOutput);
+//
+//
+// // lets now try and make a transaction bassed on the utox created above.
+// var tx = bitcore.Transaction().from(unspentOutput, publicKey);
+//
+// console.log(tx); //ok this sorta works i think were getting some where :)
+
+// now to make this usefull lets put it in functions
+
+// function to crate a utox
+
+function crateUtox(outputScript, toAddress){
+
+    //creating the data object in order to be able to create a utox
+    var data = new Object(); // creating the data opbject to create an unspent tx
+    data.txid ='00baf6626abc2df808da36a518c69f09b0d2ed0a79421ccfde4f559d2e42128b'; // {String} the previous transaction id
+    //data.txId = '00baf6626abc2df808da36a518c69f09b0d2ed0a79421ccfde4f559d2e42128b'; // {String=} alias for 'txid'
+    //data.vout = 0; // {number} the index in the transaction
+    data.outputIndex = 0; // {number=} alias for 'vout'
+    //data.scriptPubKey = outputScript; // {string|Script} the script that must be resolved to release the funds
+    data.script = outputScript; // {string|Script=} alias for 'scriptPubKey' (Output Script)
+    //data.amount = 1; // {number} amount of bitcoins associated
+    data.satoshis =100000000; // {number=} alias for 'amount', but expressed in satoshis (1 BTC = 1e8 satoshis)
+    //data.address = toAddress; // {sting | Address=} the associated address to the script, if provided.
+
+    var utox = bitcore.Transaction.UnspentOutput(data);
+    return utox;
+}
+
+
+/*  pay to pubkey script format for reference
+ *  scriptPubKey: <pubKey> OP_CHECKSIG
+ *  scriptSig: <sig>
+ */
+
+var outputScript = new bitcore.Script();
+outputScript.add(publicKey.toBuffer())
+            .add(bitcore.Opcode.OP_CHECKSIG);
+
+console.log('OutputScrip:\n'+outputScript);
+
+//verified that the above method is equvelant to my manual method
+//outputscript using script builder
+// var outputScriptFromScriptBuilder = bitcore.Script.buildPublicKeyOut(publicKey);
+// console.log('Outputscript from scriptbuilder: '+ outputScriptFromScriptBuilder);
+
+
+var utox = crateUtox(outputScript, address);
+
+// creating a JS object of a transaction
+
+// var txObject = {
+//     'adress' : address,
+//     'txID' : '00baf6626abc2df808da36a518c69f09b0d2ed0a79421ccfde4f559d2e42128c',
+//     'outputIndex' : 0,
+//     'script' : bitcore.Script.empty(),
+//     'satoshis' : 100000000
+// };
+//
+// var transx = bitcore.Transaction().fromObject(txObject);
+//
+// console.log(transx);
+//
+// var tx3 = new bitcore.Transaction();
+//
+// console.log(tx3);
+//
+// var sig = tx3.sign(privateKey);
+//
+// console.log('sig:\n'+sig.toString());
+
+// atempt 2 at creating a transaction object
+// first we create an input object
+// var inputObj = new Object();
+// inputObj.output = ''; // params.output not sure what goes here
+// inputObj.prevTxId = '0000000000000000000000000000000000000000000000000000000000000000'; //lets pretend there was no previous tx
+// inputObj.outputIndex = ''; //params.outputIndex
+// inputObj.sequenceNumber = "ffffffff"; // not sure what exactly is requiered here, stole sequence from genesis block.
+// inputObj.script = outputScript;
+//
+// var input = bitcore.Transaction.Input.fromObject(inputObj);
+// console.log('Input:\n'+input);
+
+
+// attempt 3 at building a tx
+var tx3 =  bitcore.Transaction()
+    .from(utox,publicKey)
+    .to(address, 100000000) // this has something to do with the tx3.outputs[0] this automaticaly gives me a pay to pubkey output script i think
+    .change(changeAddress);
+
+tx3.sign(privateKey, 1); // default is SIGHASH_ALL, other options include bitcore.Transaction.SIGHASH_NONE =1
+
+// //trying to extract the signature
+// var sigArr = tx3.getSignatures(privateKey,1);
+// var sig = sigArr[0].signature;
+// console.log('sig arr :');
+// console.log(sigArr);
+// console.log(sig);
+//
+// // build a script using buildpublicKeyIn
+// var signedScriptFromBuilder = bitcore.Script.buildPublicKeyIn(sig,1);
+// console.log(signedScriptFromBuilder.toString());
+
+console.log('Transaction tx3: \n');
+console.log(tx3);
+console.log('input script with sig');
+var tx3ScriptWithSig = bitcore.Script(tx3.inputs[0]._script);
+console.log(tx3ScriptWithSig.toString());
+
+// console.log('TX 3 get signature: \n');   // the signatures match even when signing a second time. Sining a tx causes the signature to be removed.
+// var sigArr = tx3.getSignatures(privateKey);
+// console.log(sigArr[0].signature.toString());
+
+
+// var tx =  bitcore.Transaction().from(utox,publicKey);
+// tx.to(address, 1000); // defining to which address satoshies will be sent.
+// console.log('Transactiong tx:');
+// console.log(tx);
+
+
+// var sig = tx3.getSignatures(privateKey, bitcore.Transaction.SIGHASH_NONE); //here we create the signature,
+//
+// tx3.applySignature(sig[0]); //here we have to apply the signature to the tx for some reason this is not doing anything
+// console.log('tx3 after sig is applied');
+// console.log(tx3);
+//
+// console.log('sig: ');
+// console.log(sig[0]); // for some reason r and s of the signature object are empty. i dont know where they come from.
+// console.log('sig to string: ');
+//console.log(sig[0].toString());
+
+// console.log(tx3.isFullySigned());
+// console.log(tx3.isValidSignature(sig));
+
+
+// to spend the transaction i need a new transaction which will the satoshies i recieved from tx3
+console.log('input script with sig');
+var tx3ScriptWithSig = bitcore.Script(tx3.inputs[0]._script);
+console.log(tx3ScriptWithSig);
+
+console.log('signature');
+var sigArr = tx3.getSignatures(privateKey,1);
+console.log(sigArr[0]);
+var scriptSig = bitcore.Script.buildPublicKeyIn(sigArr[0].signature.toDER(),1);
+console.log('script sig\n'+scriptSig);
+
+console.log('output scripts of tx3');
+// var tx3InputScript = bitcore.Script(tx3.outputs[0]._script);
+// console.log(tx3InputScript.toString()); // why am i getting a pay to pubkey hash script here OP_DUP OP_HASH160 <pubKeyHash> OP_EQUALVERIFY OP_CHECKSIG
+var tx3OutputScript = bitcore.Script(tx3.inputs[0].output._script);// expected op codes 33 (bites to be pushed onto stack), 172 OP_CHECKSIG
+console.log(tx3OutputScript.toString());
+
+
+//
+// var inputscript = new bitcore.Script();
+//
+//
+// console.log('Inputscript:\n');
+// console.log(inputscript);
+
+//nin - index of the transaction input containing the scriptSig verified.
+var nin = 0;
+var result = bitcore.Script.Interpreter().verify(tx3ScriptWithSig,outputScript,tx3); //this is correct , scripts are correct, problem could be in the interpreter. .... lets debug to find out
+
+
+console.log('result test: '+result);
+
+
+// // from interpreter ln 1114 op_checksig
+// var verify = tx3.verify();
+// console.log(verify);
+
+
+// var txOP_RETURN = new bitcore.Transaction()
+//     .from(utox)
+//     .addData('bitcoin rocks')
+//     .sign(privateKey);
+//
+// console.log('txOP_RETURNS');
+// console.log(txOP_RETURN);
+
+//
+// //testing arrays in javascript
+//
+// var testArr = new Array();
+//
+// for (var i=0; i<10;i++){
+//     testArr[i] = new Array();
+//
+//     for (var j=0; j<10;j++){
+//         testArr[i][j]= j;
+//     }
+// }
+
+
+var hash = bitcore.crypto.Hash.sha1(publicKey.toBuffer());
+
+console.log(hash);
+
+var hashStr = bitcore.crypto.BN.fromScriptNumBuffer(hash, true, hash.length);
+console.log(hash.toString('hex'));
+
+var hashScript = bitcore.Script().add(hash);
+console.log(hashScript.toString());
+
+
+console.log('is script standard?: '+ outputScript.isStandard());
+
+
+
+//test with strings to buffer
+// var str = 'hello world';
+// var strBuf = convertStringToBuffer(str);
+// console.log(strBuf);
+// console.log(bitcore.crypto.Hash.sha1(strBuf).toString('hex'));
+
+
+//
+
+// expanding parser to recognize strings
+
+var script_text = 'some op codes here  some more op codes';
+
+var opcode_arr = script_text.split(/""|\r+/);
+
+
+console.log(opcode_arr);
+
+
+var testScript = bitcore.Script();
+var stB = new bitcore.deps.Buffer(script_text);
+testScript.add(stB);
+console.log(stB);
+console.log(publicKey.toBuffer());
+console.log(testScript);
+
+
+
+
+
+// var stringToTest = 'hladfsfj328974;23#$%@#$^';
+
+// var base64str = Base64Encode(stringToTest);
+
+// console.log(base64str);
+
+// var utox = P$.createUtox(bitcore.Script());
+// console.log(utox);
+// return [new bitcore.TransactionSignature({
+//     publicKey: publicKey,
+//     prevTxId: this.prevTxId,
+//     outputIndex: this.outputIndex,
+//     inputIndex: index,
+//     signature: Sighash.sign(transaction, privateKey, sigtype, index, this.output.script),
+//     sigtype: sigtype
+// })];
+
+// overwriting the get sig method todo over write it properly
+
+
+
+
+
+console.log('proto changed');
+
